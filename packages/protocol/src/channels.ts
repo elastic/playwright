f3441b1d93091725ba929e2ec8dbc70cefc081ef
kyungeunni <kyungeunni@gmail.com>
2022-07-21 18:00:23 +0200

    modify and export recorder

--- packages/protocol/src/channels.ts
+++ packages/protocol/src/channels.ts
@@ -1536,6 +1536,8 @@ export type BrowserContextRecorderSupplementEnableParams = {
   outputFile?: string,
   handleSIGINT?: boolean,
   omitCallTracking?: boolean,
+  showRecorder?: boolean,
+  actionListener?: any,
 };
 export type BrowserContextRecorderSupplementEnableOptions = {
   language?: string,
@@ -1548,6 +1550,8 @@ export type BrowserContextRecorderSupplementEnableOptions = {
   outputFile?: string,
   handleSIGINT?: boolean,
   omitCallTracking?: boolean,
+  showRecorder?: boolean,
+  actionListener?: any,
 };
 export type BrowserContextRecorderSupplementEnableResult = void;
 export type BrowserContextNewCDPSessionParams = {
